abc42617c7c84da301b8ced9abcf8591fdb11fe0
malhotra-parul <parulm.business@gmail.com>
2020-08-18 23:42:35 +0530

    modified structure for deployment

--- server/index.js
+++ server/index.js
@@ -53,5 +53,5 @@ app.post("/authenticate", (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
