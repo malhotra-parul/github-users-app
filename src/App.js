c0385bb553d791ec3011a8556b9aaf9c44927fde
malhotra-parul <parulm.business@gmail.com>
2020-08-18 06:48:53 +0530

    created Home, Login routes and AuthContext for Oauth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
