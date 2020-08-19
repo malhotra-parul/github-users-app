e1a68ab181c7ce86e1024553f5d631d72a43a017
malhotra-parul <parulm.business@gmail.com>
2020-08-19 08:14:26 +0530

    changes made for deployment

--- index.js
+++ index.js
@@ -54,4 +54,9 @@ app.post("/authenticate", (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
