c478ae4d9198f3698b19f49f64501b946ffe5ea6
malhotra-parul <parulm.business@gmail.com>
2020-08-27 17:48:42 +0530

    updated env

--- client/src/context/Github/GithubState.js
+++ client/src/context/Github/GithubState.js
@@ -14,6 +14,7 @@ import {
   SET_DISABLED
 } from "../../types";
 
+//updated env file
 const GithubState = (props) => {
   const initialState = {
     currentUser: JSON.parse(localStorage.getItem("currentUser")) || null,
